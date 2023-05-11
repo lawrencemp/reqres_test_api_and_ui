12f030737c9e4e00cde4283ccf5e5f5d3183b349
DefaultUser_2.0 <lawrencemp@yandex.ru>
2023-05-11 17:30:04 +0300

    Initial commit

---
+++ conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+from apis.reqres_api import ApiClient
+
+
+@pytest.fixture(scope="function")
+def browser():
+    browser = webdriver.Chrome()
+    yield browser
+    browser.quit()
+
+
+@pytest.fixture(scope="function")
+def api_calling():
+    return ApiClient
+
+
+
+
---
+++ pages/base_page.py
@@ -0,0 +1,27 @@
+from selenium.common import NoSuchElementException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+
+class BasePage:
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def get_element(self, how, what, timeout=4):
+        return WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+
+    def scroll_into_element(self, how, what):
+        elem = self.browser.find_element(how, what)
+        self.browser.execute_script("arguments[0].scrollIntoView(true);", elem)
+
+    def is_element_present(self, how, what, timeout=6):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except NoSuchElementException:
+            return False
+        return True
